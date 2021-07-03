19b5b97a94a98f9d1efe379ad028c7ef45078223
edward@kiwi <edwardyu@outlook.com>
2021-07-03 10:07:50 -0700

    style

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-const cacheName = 'greener-v003';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
+const cacheName = 'greener-v004';
+const filesToCache = [];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+self.addEventListener('install', function (e) {});
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
