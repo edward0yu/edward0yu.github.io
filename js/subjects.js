072f9b22fa0f35ef3768193bcfea2e743565d066
edward@kiwi <edwardyu@outlook.com>
2021-08-17 13:34:41 -0700

    student

---
+++ js/subjects.js
@@ -0,0 +1,24 @@
+const subjects = [
+  [
+    "Math:",
+    "Elementory School Math",
+    "Middle School Math",
+    "High School Math"
+  ],
+  [
+    "English:",
+    "Elementory School English",
+    "Middle School English",
+    "High School English"
+  ],
+  [
+    "Science:",
+    "Middle School Science",
+    "High School Science"
+  ],
+  [
+    "Social Studies:",
+    "Middle School Social Studies",
+    "High School Social Studies"
+  ]
+];
